3c62a47876fabd3243ff4a13774ecb301f15d7b3
virginiadelval <54652251+virginiadelval@users.noreply.github.com>
2024-10-10 23:54:54 -0300

    mapa final de Home Index

--- js/mapa.js
+++ js/mapa.js
@@ -14,167 +14,187 @@ var argenmap = L.tileLayer('https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0
 	attribution: '<a href="https://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2"  target="_blank"> ArgenMap </a>',
 	maxZoom: 18,
 });
-
-
-
-var Localidad = L.geoJSON(locCabecera, {
-	pointToLayer: function (feature, latlng) {
-		return L.circleMarker(latlng, IconLocCab);
+var PROVFAO = L.geoJSON(provinciasFAO, {
+	style: function (feature) {
+		return {
+			fillColor: "gray",
+			weight: 1,
+			opacity: 1,
+			color: '#000',
+			dashArray: '0.2',
+			fillOpacity: 0.1
+		};
 	},
-	onEachFeature: function (feature, layer) {
-		var content = popupContentLocCab(feature);
-		layer.bindPopup(content);
-	}
-});
-var Parajes = L.geoJSON(locSec, {
-	pointToLayer: function (feature, latlng) {
-		return L.circleMarker(latlng, IconLocSec);
-	},
-	onEachFeature: function (feature, layer) {
-		var content = popupContentLocSec(feature);
-		layer.bindPopup(content);
-	}
+	
 });
 
-var POBIndigena = L.geoJSON(pobInd, {
-	pointToLayer: function (feature, latlng) {
-		return L.circleMarker(latlng, IconPobInd);
-	},
+var REFIONESFAO = L.geoJSON(regionesFAO, {
+	style: styleRegiones,
 	onEachFeature: function (feature, layer) {
-		var content = popupContentPobInd(feature);
-		layer.bindPopup(content);
-	}
-});
-
-//data de Nueclos de Agricultura faminial (extraido del Atlas de AF  - Horacio)
-var NucleosAF = L.geoJSON(naf, {
-	style: styleNAG,
-	onEachFeature: function (feature, layer) {
-		var content = popupContentNAF(feature);
-		layer.bindPopup(content);
-	}
-});
-
-//data de Nueclos de Agricultura faminial (extraido del Atlas de AF  - Horacio)
-var NBI = L.geoJSON(nbi, {
-	style: styleNBI,
-	onEachFeature: function (feature, layer) {
-		var content = popupContentNBI(feature);
-		layer.bindPopup(content);
-	}
-});
-
-var AguaRED = L.geoJSON(aguaRed, {
-	style: styleAguaRed,
-	onEachFeature: function (feature, layer) {
-		var content = popupContentAguaRed(feature);
+		var content = popupContentRegiones(feature);
 		layer.bindPopup(content);
 	}
 });
 
 
+// var Localidad = L.geoJSON(locCabecera, {
+// 	pointToLayer: function (feature, latlng) {
+// 		return L.circleMarker(latlng, IconLocCab);
+// 	},
+// 	onEachFeature: function (feature, layer) {
+// 		var content = popupContentLocCab(feature);
+// 		layer.bindPopup(content);
+// 	}
+// });
+// var Parajes = L.geoJSON(locSec, {
+// 	pointToLayer: function (feature, latlng) {
+// 		return L.circleMarker(latlng, IconLocSec);
+// 	},
+// 	onEachFeature: function (feature, layer) {
+// 		var content = popupContentLocSec(feature);
+// 		layer.bindPopup(content);
+// 	}
+// });
 
-//Datos de Localidades con Proyectos de Agua, Estandarizado y No Estandarizado - Generado a partir del Excel con todos los proyectos.
-var ProyecAgua = L.geoJSON(proyectos,
-	{
-		pointToLayer: function (feature, latlng) {
-			return L.circleMarker(latlng, MarkerOptions);
+// var POBIndigena = L.geoJSON(pobInd, {
+// 	pointToLayer: function (feature, latlng) {
+// 		return L.circleMarker(latlng, IconPobInd);
+// 	},
+// 	onEachFeature: function (feature, layer) {
+// 		var content = popupContentPobInd(feature);
+// 		layer.bindPopup(content);
+// 	}
+// });
 
-		},
-		style: styleProyectos,
-		onEachFeature: function (feature, layer) {
-			var content = popupContentProyectos(feature);
-			layer.bindPopup(content);
-		}
-	});
+// //data de Nueclos de Agricultura faminial (extraido del Atlas de AF  - Horacio)
+// var NucleosAF = L.geoJSON(naf, {
+// 	style: styleNAG,
+// 	onEachFeature: function (feature, layer) {
+// 		var content = popupContentNAF(feature);
+// 		layer.bindPopup(content);
+// 	}
+// });
 
-// Data poblacional, del año 2022 por departamento. Tomado de: https://portalgeoestadistico.indec.gob.ar/?indicator_id=37&members=108
-var DataCenso = L.geoJSON(censo,
-	{
-		style: styleCenso,
-		onEachFeature: function (feature, layer) {
-			var content = popupContentCenso(feature);
-			layer.bindPopup(content);
-		}
-	});
+// //data de Nueclos de Agricultura faminial (extraido del Atlas de AF  - Horacio)
+// var NBI = L.geoJSON(nbi, {
+// 	style: styleNBI,
+// 	onEachFeature: function (feature, layer) {
+// 		var content = popupContentNBI(feature);
+// 		layer.bindPopup(content);
+// 	}
+// });
 
-///Dapto con Arsenicos segun el Ministerio de Salud en 2006 https://www.argentina.gob.ar/sites/default/files/2006_epidemiologia_del_hacre_en_argentina.pdf
-var DeptoArsenico = L.geoJSON(dptoArsenico,
-	{
-		style: styleDptoArsenico,
-		onEachFeature: function (feature, layer) {
-			var content = popupContentDptoArsenico(feature);
-			layer.bindPopup(content);
-		}
-	});
+// var AguaRED = L.geoJSON(aguaRed, {
+// 	style: styleAguaRed,
+// 	onEachFeature: function (feature, layer) {
+// 		var content = popupContentAguaRed(feature);
+// 		layer.bindPopup(content);
+// 	}
+// });
 
-///Daptos del IGN
-var RegionesHirdo = L.geoJSON(RegionHidrogeografica,
-	{
-		 style: styleRegionHidro,
-		 onEachFeature: function (feature, layer) {
-			var content = popupContentRegionHidro(feature);
-			layer.bindPopup(content);
-		}  
-	}  
-);
 
-///Dapto IGN
-var CuencasHidro = L.geoJSON(cuencasSist,
-	// {
-	// 	 style: styleRegionHidro,
+
+// //Datos de Localidades con Proyectos de Agua, Estandarizado y No Estandarizado - Generado a partir del Excel con todos los proyectos.
+// var ProyecAgua = L.geoJSON(proyectos,
+// 	{
+// 		pointToLayer: function (feature, latlng) {
+// 			return L.circleMarker(latlng, MarkerOptions);
+
+// 		},
+// 		style: styleProyectos,
+// 		onEachFeature: function (feature, layer) {
+// 			var content = popupContentProyectos(feature);
+// 			layer.bindPopup(content);
+// 		}
+// 	});
+
+// // Data poblacional, del año 2022 por departamento. Tomado de: https://portalgeoestadistico.indec.gob.ar/?indicator_id=37&members=108
+// var DataCenso = L.geoJSON(censo,
+// 	{
+// 		style: styleCenso,
+// 		onEachFeature: function (feature, layer) {
+// 			var content = popupContentCenso(feature);
+// 			layer.bindPopup(content);
+// 		}
+// 	});
+
+// ///Dapto con Arsenicos segun el Ministerio de Salud en 2006 https://www.argentina.gob.ar/sites/default/files/2006_epidemiologia_del_hacre_en_argentina.pdf
+// var DeptoArsenico = L.geoJSON(dptoArsenico,
+// 	{
+// 		style: styleDptoArsenico,
+// 		onEachFeature: function (feature, layer) {
+// 			var content = popupContentDptoArsenico(feature);
+// 			layer.bindPopup(content);
+// 		}
+// 	});
+
+// ///Daptos del IGN
+// var RegionesHirdo = L.geoJSON(RegionHidrogeografica,
+// 	{
+// 		 style: styleRegionHidro,
+// 		 onEachFeature: function (feature, layer) {
+// 			var content = popupContentRegionHidro(feature);
+// 			layer.bindPopup(content);
+// 		}  
+// 	}  
+// );
+
+// ///Dapto IGN
+// var CuencasHidro = L.geoJSON(cuencasSist,
+// 	// {
+// 	// 	 style: styleRegionHidro,
 		
-	// }
-);
+// 	// }
+// );
 
-var SitiosSeleccionados = L.geoJSON(sitios,
-	{
-		pointToLayer: function (feature, latlng) {
-			return L.circleMarker(latlng, MarkerOptionsSitios ,
-			);
+// var SitiosSeleccionados = L.geoJSON(sitios,
+// 	{
+// 		pointToLayer: function (feature, latlng) {
+// 			return L.circleMarker(latlng, MarkerOptionsSitios ,
+// 			);
 
-		},
- 	style: MarkerOptionsSitios ,
-	 onEachFeature: function (feature, layer) {
-		var content = popupContentSitios(feature);
-		layer.bindPopup(content);
-	}
+// 		},
+//  	style: MarkerOptionsSitios ,
+// 	 onEachFeature: function (feature, layer) {
+// 		var content = popupContentSitios(feature);
+// 		layer.bindPopup(content);
+// 	}
 		
-	}
-);
+// 	}
+// );
 
-var EstacionesSMN = L.geoJSON(SMN,
-	{
-		pointToLayer: function (feature, latlng) {
-			return L.circleMarker(latlng, MarkerOptionsSMN,
-			);
+// var EstacionesSMN = L.geoJSON(SMN,
+// 	{
+// 		pointToLayer: function (feature, latlng) {
+// 			return L.circleMarker(latlng, MarkerOptionsSMN,
+// 			);
 
-		},
- 	style: MarkerOptionsSMN,
-	 onEachFeature: function (feature, layer) {
-		var content = popupContentSMN(feature);
-		layer.bindPopup(content);
-	}
+// 		},
+//  	style: MarkerOptionsSMN,
+// 	 onEachFeature: function (feature, layer) {
+// 		var content = popupContentSMN(feature);
+// 		layer.bindPopup(content);
+// 	}
 		
-	}
-);
+// 	}
+// );
 
-var SuelosTipo = L.geoJSON(suelos,
-	{
-		style: styleSuelos,
-		onEachFeature: function (feature, layer) {
-			var content = popupContentSuelos(feature);
-			layer.bindPopup(content);
-		}  
-	}
-);
+// var SuelosTipo = L.geoJSON(suelos,
+// 	{
+// 		style: styleSuelos,
+// 		onEachFeature: function (feature, layer) {
+// 			var content = popupContentSuelos(feature);
+// 			layer.bindPopup(content);
+// 		}  
+// 	}
+// );
 
-var Ecoregion = L.geoJSON(ecorregion,
-	{
-		style: styleEco,
-		onEachFeature: function (feature, layer) {
-			var content = popupContentEcoregion(feature);
-			layer.bindPopup(content);
-		}  
-	}
-);
+// var Ecoregion = L.geoJSON(ecorregion,
+// 	{
+// 		style: styleEco,
+// 		onEachFeature: function (feature, layer) {
+// 			var content = popupContentEcoregion(feature);
+// 			layer.bindPopup(content);
+// 		}  
+// 	}
+// );
--- js/panelLayers.js
+++ js/panelLayers.js
@@ -13,189 +13,152 @@ var baseLayers = [
 		layer: google
 	}];
 // Grupos de capas interactivas
+// Grupos de capas interactivas
 var overlayMaps = [
 	{
-		group: "Información de base",
+		group: "Jurisdicciones",
 		collapsed: true,
 		layers: [
+			{
+				active: false,
+				name: "Regiones",
+				layer: REFIONESFAO,
+				transparent: true,
+				opacity: 0.5,
+			}, {
+				active: false,
+				name: "Provincias",
+				layer: PROVFAO,
+				transparent: true,
+				opacity: 0.5,
+			},
 			// 
-			 {
-				active: false,
-				name: "Localidades Censales",
-				layer: Localidad,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "Parajes",
-				layer: Parajes,
-				transparent: true,
-				opacity: 0.5,
-			},
-
-
-		{
-				active: false,
-				name: "Pob. Total (2022)",
-				layer: DataCenso,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "Pobl. Originaria (2022)",
-				layer: POBIndigena,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "% de Hogares con NBI (2010)",
-				layer: NBI,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "% de Hogares con Agua de Red (2022)",
-				layer: AguaRED,
-				transparent: true,
-				opacity: 0.5,
-			},{
-				active: false,
-				name: "Pob. con Nucleos de Agricultura Familiar",
-				layer: NucleosAF,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "Niveles de Arsenico (ppb)",
-				layer: DeptoArsenico,
-				transparent: true,
-				opacity: 0.5,
-			},
-			
-			
-
-		]
-	},
-	{
-		group: "Proyectos de Agua",
-		collapsed: true,
-		layers: [
-			{
-				active: false,
-				name: "Proyectos Estandarizados <br> y No Estandarizados",
-				layer: ProyecAgua,
-				transparent: true,
-				opacity: 0.5,
-			},
-			
-
-
-		]
-	},
-	{
-		group: "Modelos de Cosecha de Agua",
-		collapsed: true,
-		layers: [
-			{
-				active: false,
-				name: "Sitios seleccionados",
-				layer: SitiosSeleccionados,
-				transparent: true,
-				opacity: 0.5,
-			},
-			
-
-
-		]
-	},
-	
-	
-	
-	
-
-
-];
-//panel ambiental
-var infoAmb = [
-	{
-		group: "Información Ambiental",
-		collapsed: true,
-		layers: [
 			// {
 			// 	active: false,
-			// 	name: "Rios",
-			// 	layer: Rios,
+			// 	name: "Localidades Censales",
+			// 	layer: Localidad,
 			// 	transparent: true,
 			// 	opacity: 0.5,
 			// },
-			{
-				active: false,
-				name: "Estaciones Meteorologicas",
-				layer: EstacionesSMN,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "Suelos",
-				layer: SuelosTipo,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "Ecoregion",
-				layer: Ecoregion,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "Cuencas Hídricas",
-				layer: CuencasHidro,
-				transparent: true,
-				opacity: 0.5,
-			},
-			{
-				active: false,
-				name: "Regiones Hidrogeográficas",
-				layer: RegionesHirdo,
-				transparent: true,
-				opacity: 0.5,
-			},
+			// {
+			// 	active: false,
+			// 	name: "Parajes",
+			// 	layer: Parajes,
+			// 	transparent: true,
+			// 	opacity: 0.5,
+			// },
 
-			{
-				active: false,
-				name: "Unidades Geológicas (2.5M)",
-				layer: GeologicoIGN,
-				transparent: true,
-				opacity: 0.5,
-			},
-			
+
 		]
 	},
 	// {
-	// 	group: "Arsénico",
+	// 	group: "Info Censal - INDEC",
 	// 	collapsed: true,
 	// 	layers: [
+
 	// 		{
 	// 			active: false,
-	// 			name: "Dptos con Niveles de Arsenico",
-	// 			layer: DeptoArsenico,
+	// 			name: "Datos Censales (2022)",
+	// 			layer: DataCenso,
+	// 			transparent: true,
+	// 			opacity: 0.5,
+	// 		},
+	// 		{
+	// 			active: false,
+	// 			name: "Pobl. con NBI (2010)",
+	// 			layer: NBI,
+	// 			transparent: true,
+	// 			opacity: 0.5,
+	// 		},
+	// 		{
+	// 			active: false,
+	// 			name: "Pobl. Originaria (2010)",
+	// 			layer: POBIndigena,
 	// 			transparent: true,
 	// 			opacity: 0.5,
 	// 		},
 
-
 	// 	]
 	// }
 
+
 ];
+//panel ambiental
+// var infoAmb = [
+// 	{
+// 		group: "Información Ambiental",
+// 		collapsed: true,
+// 		layers: [
+// 			// {
+// 			// 	active: false,
+// 			// 	name: "Rios",
+// 			// 	layer: Rios,
+// 			// 	transparent: true,
+// 			// 	opacity: 0.5,
+// 			// },
+// 			{
+// 				active: false,
+// 				name: "Estaciones Meteorologicas",
+// 				layer: EstacionesSMN,
+// 				transparent: true,
+// 				opacity: 0.5,
+// 			},
+// 			{
+// 				active: false,
+// 				name: "Suelos",
+// 				layer: SuelosTipo,
+// 				transparent: true,
+// 				opacity: 0.5,
+// 			},
+// 			{
+// 				active: false,
+// 				name: "Ecoregion",
+// 				layer: Ecoregion,
+// 				transparent: true,
+// 				opacity: 0.5,
+// 			},
+// 			{
+// 				active: false,
+// 				name: "Cuencas Hídricas",
+// 				layer: CuencasHidro,
+// 				transparent: true,
+// 				opacity: 0.5,
+// 			},
+// 			{
+// 				active: false,
+// 				name: "Regiones Hidrogeográficas",
+// 				layer: RegionesHirdo,
+// 				transparent: true,
+// 				opacity: 0.5,
+// 			},
+
+// 			{
+// 				active: false,
+// 				name: "Unidades Geológicas (2.5M)",
+// 				layer: GeologicoIGN,
+// 				transparent: true,
+// 				opacity: 0.5,
+// 			},
+			
+// 		]
+// 	},
+// 	// {
+// 	// 	group: "Arsénico",
+// 	// 	collapsed: true,
+// 	// 	layers: [
+// 	// 		{
+// 	// 			active: false,
+// 	// 			name: "Dptos con Niveles de Arsenico",
+// 	// 			layer: DeptoArsenico,
+// 	// 			transparent: true,
+// 	// 			opacity: 0.5,
+// 	// 		},
+
+
+// 	// 	]
+// 	// }
+
+// ];
 
 
 // panel de CC
@@ -358,12 +321,12 @@ var panelLayers1 = new L.Control.PanelLayers(null, overlayMaps,
 		position: 'topleft',
 	});
 
-var panelLayers2 = new L.Control.PanelLayers(null, infoAmb,
-		{
-			compact: true,
-			collapsed: false,
-			collapsibleGroups: true,
-			position: 'topleft',
-		});
+// var panelLayers2 = new L.Control.PanelLayers(null, infoAmb,
+// 		{
+// 			compact: true,
+// 			collapsed: false,
+// 			collapsibleGroups: true,
+// 			position: 'topleft',
+// 		});
 
 
